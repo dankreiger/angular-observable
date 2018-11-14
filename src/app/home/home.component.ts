331c31614f6437a00a3cf76e529255f2cba0bd3a
Daniel Kreiger <d.kreiger@compuccino.com>
2018-11-14 13:34:34 +0100

    aight

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,15 +9,16 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 export class HomeComponent implements OnInit, OnDestroy {
   private numbersObsSubscription: Subscription;
   private customObsSubscription: Subscription;
+  public puppy: number;
   constructor() { }
 
   ngOnInit() {
 
     /* infinite */
-    const myNumbers = interval(1000);
+    const myNumbers = interval(10);
     this.numbersObsSubscription = myNumbers.subscribe(
       (number: number) => {
-        console.log(number);
+        this.puppy = number;
       }
     );
     const myObservable = Observable.create((observer: Observer<string>) => {
